c86f964e51be9ffa7d15e54f663334b91d5eb606
Charles d'Harcourt <cmwar596@dharcourt.com>
2014-01-18 22:20:16 -0600

    Used a simpler gentler version of Dart localization.
    Full-on Dart Intl localization with dart-to-json parsing followed by
    localization and json-to-dart code generation is overkill for this
    example and makes it way too complex, so it was replaced with simpler
    code that leverages Intl for its DateFormat and NumberFormat
    functionality but not for its messages or plural localization one.

--- tabs.dart
+++ tabs.dart
@@ -1,26 +1,9 @@
 import 'dart:async';
-import 'messages_all.dart';
+import 'dart:math';
 import 'package:angular/angular.dart';
 import 'package:di/di.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-
-@NgComponent(
-    selector: 'pane',
-    templateUrl: 'pane.html',
-    cssUrl: 'pane.css',
-    applyAuthorStyles: true,
-    publishAs: 'pane',
-    map: const {'title' : '@'}
-)
-class PaneComponent {
-  String title = '';
-  bool selected = false;
-
-  PaneComponent(TabsComponent tabs) {
-    tabs.addPane(this);
-  }
-}
 
 @NgComponent(
     selector: 'tabs',
@@ -44,55 +27,58 @@ class TabsComponent {
   }
 }
 
+@NgComponent(
+    selector: 'pane',
+    templateUrl: 'pane.html',
+    cssUrl: 'pane.css',
+    applyAuthorStyles: true,
+    publishAs: 'pane',
+    map: const {'title' : '@'}
+)
+class PaneComponent {
+  String title = '';
+  bool selected = false;
+
+  PaneComponent(TabsComponent tabs) {
+    tabs.addPane(this);
+  }
+}
+
 @NgController(
     selector: '[tabs-controller]',
     publishAs: 'controller')
 class TabsController {
   List<int> beerCounts = [0, 1, 2, 3, 4, 5, 6];
-  String locale;
   DateFormat dateFormat;
-  NumberFormat currencyFormat;
   NumberFormat numberFormat;
+  NumberFormat currencyFormat;
+  List<String> beerMessages;
 
   TabsController(): this.withLocale('sk');
 
   TabsController.withLocale(String locale) {
-    this.locale = Intl.canonicalizedLocale(locale);
-    dateFormat = new DateFormat(DateFormat.YEAR_MONTH_WEEKDAY_DAY, this.locale);
-    currencyFormat =
-        new NumberFormat(Intl.message(
-            '\$#,##0.00',
-            name: 'currency_format',
-            args: [],
-            desc: 'NumberFormat format string for currency amounts',
-            locale: this.locale), this.locale);
-    numberFormat =
-        new NumberFormat(Intl.message(
-            '#,##0.###',
-            name: 'number_format',
-            args: [],
-            desc: 'NumberFormat format string for non-currency numbers',
-            locale: this.locale), this.locale);
+    locale = Intl.canonicalizedLocale(locale);
+    this.dateFormat = new DateFormat('yMMMMEEEEd', locale);
+    this.numberFormat = new NumberFormat('#,##0.###', locale);
+    if (locale == 'sk') {
+      this.currencyFormat = new NumberFormat('#,##0.00 \u20AC');
+      this.beerMessages = ['\u017Eiadne pivo', '{} pivo', '{} piv\u00E1'];
+      this.beerMessages.addAll(['{} piv\u00E1', '{} piv\u00E1', '{} p\u00EDv']);
+    } else {
+      this.currencyFormat = new NumberFormat('\$#,##0.00');
+      this.beerMessages = ['no beers', '{} beer', '{} beers'];
+    }
   }
 
-  String beerCountMessage(int count) =>
-      Intl.plural(
-          count,
-          zero: 'no beers',
-          one: '$count beer',
-          few: '$count beers',
-          other: '$count beers',
-          args: [count],
-          name: 'beer_count_message',
-          desc: 'Indicates how many beers there are',
-          locale: this.locale);
+  String formattedDate(String iso) => dateFormat.format(DateTime.parse(iso));
 
-  String formattedDate(String isoDate) =>
-      dateFormat.format(DateTime.parse(isoDate));
+  String formattedNumber(num number) => numberFormat.format(number);
 
   String formattedCurrency(int amount) => currencyFormat.format(amount);
 
-  String formattedNumber(num number) => numberFormat.format(number);
+  String beerCountMessage(int count) =>
+      this.beerMessages[min(count, this.beerMessages.length - 1)]
+          .replaceAll('{}', count.toString());
 }
 
 class TabsModule extends Module {
@@ -104,9 +90,7 @@ class TabsModule extends Module {
 }
 
 main() {
-  Future.wait([initializeMessages('en'),
-               initializeDateFormatting('en', null),
-               initializeMessages('sk'),
+  Future.wait([initializeDateFormatting('en', null),
                initializeDateFormatting('sk', null)]).then((_) {
     ngBootstrap(module: new TabsModule(), selector: '#tabs-app');
   });
